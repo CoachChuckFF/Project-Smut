79c7e9c2004668a80c52f3ea609e4c8df9f9dc40
Coach Chuck <christian.krueger.health@gmail.com>
2023-08-27 17:10:54 -0500

    working

--- src/app/read/[id]/ReadPageContent.tsx
+++ src/app/read/[id]/ReadPageContent.tsx
@@ -140,7 +140,7 @@ export default function ReadPageContent(props: ReadPageContentProps) {
 
         <button
           onClick={onBack}
-          className="fixed bottom-10 right-10 bg-stone-700 hover:bg-stone-800 text-white w-12 h-12 rounded-full shadow-lg flex items-center justify-center"
+          className="fixed bottom-10 left-10 bg-stone-700 hover:bg-stone-800 text-white w-12 h-12 rounded-full shadow-lg flex items-center justify-center"
           aria-label="Back"
         >
           ←
